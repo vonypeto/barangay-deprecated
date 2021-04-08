858ba86e74ec189df056644423ae11d5f2434e5c
vonypeto <von.aralar@gmail.com>
2021-04-08 10:47:06 +0800

    Nav bar edited

--- resources/views/layouts/image.blade.php
+++ resources/views/layouts/image.blade.php
@@ -3,7 +3,7 @@
 
 
 
-    <div class="sidebar-heading text-center col-sm-12 pr-1 pl-1">{{ $image->barangay_name ?? 'Barangay no set' }}</div>
+    <div class="sidebar-heading text-center col-sm-12 pr-1 pl-1" style="width: 268px">{{ $image->barangay_name ?? 'Barangay no set' }}</div>
 
     <div class="sidebar-heading container-fluid text-center pr-0 pl-0 ">
         <img class="logo" src="{{  url($image->url ?? 'Logo not set')  }}">
