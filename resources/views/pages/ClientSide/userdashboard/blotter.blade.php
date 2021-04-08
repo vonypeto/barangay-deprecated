e34cf852f73bb16d153eea6321e701539f49079d
vonypeto <von.aralar@gmail.com>
2021-04-08 10:10:01 +0800

    edited

--- resources/views/pages/ClientSide/userdashboard/blotter.blade.php
+++ resources/views/pages/ClientSide/userdashboard/blotter.blade.php
@@ -34,13 +34,10 @@
         border-radius: 20px;
         overflow: hidden;
         box-shadow: 0 15px 25px rgba(0,0,0,0.2);
-
     }
-
     /* .card-container h4, h6{
         font-family: Arial, Helvetica, sans-serif;
     } */
-
     .card-container .card-header{
         width: 100%;
         height: 65%;
@@ -48,37 +45,30 @@
         padding-left: 1.8rem;
         padding-top: 1.2rem;
     }
-
     .card-container .card-content {
         padding: 0.5rem;
         position: relative;
     }
-
     .card-container .card-content p{
         color: black;
         font-size: 0.9rem;
-
     }
-
     .card-container .card-content .card-content-btn{
         position: absolute;
         top: 1rem;
     }
-
     .card-container .card-content .card-content-btn button{
         border-radius: 50%;
     }
 </style>
 
-<div style="margin: 30px;margin-bottom: 80px;">
-    <div class="jumbotron" style="margin-bottom: 175px;">
-        <div class="container " style="margin: 0 auto;color: white;padding: 15px;border-radius: 25px;margin-bottom: 10px;">
+<section class="contact-clean" style="padding-bottom: 140px;">
+        <div class="container " style="margin: 0 auto;color: white;padding: 15px;border-radius: 25px;">
 
             <div class="row justify-content-center">
                 @forelse ($data as $item)
-                <div class="col col-4" style="margin-bottom: 4rem">
 
-                    <div class="card-container">
+                    <div class="card-container" style="margin: 2rem;">
                         <div class="card-header">
                             <h4>{{ $item->incident_type }}</h4>
                             <h6>{{ $item->incident_location}}</h6>
@@ -93,16 +83,12 @@
                         </div>
                     </div>
 
-                </div>
                 @empty
-                    <h5 style="color: black;">You have no Blotter Report.</h5>
+                    <h4 style="color: black; margin-top:4rem;">You have no Blotter Report.</h4>
                 @endforelse
             </div>
-
-
         </div>
-    </div>
-</div>
+</section>
 
 
 
@@ -122,10 +108,7 @@
                    <tr class="headings">
                       <th class="column-title">Blotter Id</th>
                       <th class="column-title">Status</th>
-                      <th class="column-title">Incident Location</th>
-                      <th class="column-title">Incident Type</th>
-                      <th class="column-title">Incident Date</th>
-                      <th class="column-title">Incident Time</th>
+
                       <th class="column-title">Schedule Date</th>
                       {{-- <th class="column-title">Schedule Time</th> --}}
                    </tr>
@@ -134,11 +117,32 @@
                    <tr>
                       <td id="viewblotter_id"></td>
                       <td id="status"></td>
+
+                      <td id="viewschedule_date"></td>
+                      {{-- <td id="viewschedule_time"></td> --}}
+                   </tr>
+                </tbody>
+             </table>
+
+             <h5>Incident Information</h5>
+             <table  class="bulk_action dataTables_info table datatable-element table-striped jambo_table bulk_action text-center border no-footer">
+                <thead style="background: rgb(0, 87, 121); color:white;">
+                   <tr class="headings">
+
+                      <th class="column-title">Incident Location</th>
+                      <th class="column-title">Incident Type</th>
+                      <th class="column-title">Incident Date</th>
+                      <th class="column-title">Incident Time</th>
+                      {{-- <th class="column-title">Schedule Time</th> --}}
+                   </tr>
+                </thead>
+                <tbody>
+                   <tr>
+
                       <td id="viewincident_location"></td>
                       <td id="viewincident_type"></td>
                       <td id="viewdate_incident"></td>
                       <td id="viewtimeof_incident"></td>
-                      <td id="viewschedule_date"></td>
                       {{-- <td id="viewschedule_time"></td> --}}
                    </tr>
                 </tbody>
@@ -150,7 +154,6 @@
              <table id="blotter_list-table" class="bulk_action dataTables_info table datatable-element table-striped jambo_table bulk_action text-center border no-footer">
                 <thead style="background: rgb(0, 87, 121); color:white;">
                    <tr class="headings">
-                      <th class="column-title">Resident Id</th>
                       <th class="column-title">FullName</th>
                       <th class="column-title">Involvement Type</th>
                       <th class="bulk-actions" hidden colspan="7">
@@ -217,7 +220,6 @@
         </div>
     </footer>
     <script type="text/javascript">
-
 $('body').on('click', '#viewBlotter', function(){
                var blotter_id = $(this).data('id');
                $.get("{{ route('blotters.index') }}" +'/' + blotter_id +'/edit', function (data) {
@@ -229,23 +231,19 @@ $('body').on('click', '#viewBlotter', function(){
                   $('#viewincident_type').html(data[0].incident_type);
                   $('#viewdate_incident').html(data[0].date_incident);
                   $('#viewtimeof_incident').html(data[0].time_incident);
-
-
                   $('#viewschedule_date').html(data[0].schedule_date);
                   // $('#viewschedule_time').html(data[0].schedule_time);
                   $('#viewincident_narrative').val(data[0].incident_narrative);
-
                   var len = data[1].length;
                   var tbody = ' <tbody class="blotter-list-data"></tbody>';
                   if(len > 0){
                      $('.blotter-list-data').remove();
                      $('#blotter_list-table').append(tbody);
                      for(var i = 0; i <len;i++){
-                        var resident_id = data[1][i].resident_id;
+                        // var resident_id = data[1][i].resident_id;
                         var person_involve = data[1][i].person_involve;
                         var involvement_type = data[1][i].involvement_type;
-                        var tr = '<tr>'
-                        +'<td>'+ resident_id +'</td>'+
+                        var tr = '<tr>'+
                         '<td>'+ person_involve +'</td>'+
                         '<td>'+ involvement_type +'</td>'+
                         '</tr>'
@@ -257,7 +255,6 @@ $('body').on('click', '#viewBlotter', function(){
                   }
                })
              });
-
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
